c5fb927f433e77b2bd1748be6d1ae2a02f61e7a6
Anna Kravchenko <flamedance.ann@gmail.com>
2022-08-13 14:06:28 +0300

    feat: add css & js for clock

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+import { Clock } from './js/Clock';
+
 window.onload = function() {
-  console.log("Hello")
+  setInterval(getClock, 1000);
+
+  getCalendar();
 }
+
+const getClock = () => {
+  const clock = document.querySelector(".clock");
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+};
+
+const getCalendar = () => {
+  const calendar = document.querySelector(".calendar");
+  const today = new Date();
+  const year = String(today.getFullYear());
+  calendar.innerText = `${year}`;
+};
